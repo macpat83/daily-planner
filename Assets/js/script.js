92fa1079e0dc7fc511010992e9513d95d0dd98e2
macpat83 <macpat@gmail.com>
2022-04-17 10:43:41 -0500

    javascript functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,7 +5,48 @@ function getCurrentDay(){
 
 getCurrentDay();
 
-//creating hour blocks
+$(init);
 
+function init() {
+  // get current day and display on top of page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+  // color our time blocks and start interval to re-color every minute
+  colorHourBlocks();
+  setInterval(colorHourBlocks, 60000);
 
+  // update hour with data in local storage
+  $(".hour-block").each(function() {
+    var blockId = $(this).attr("id");
+    // load saved data from local storage
+    $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+  });
+
+  // attach our handler for the save buttons
+  $(".saveBtn").on("click", handleSave);
+}
+
+function colorHourBlocks() {
+  // for each hour block
+  $(".hour-block").each(function() {
+    var pastHour = parseInt($(this).attr("id").replace("hour-", ""));
+    var currentHour = parseInt(moment().format("H"));
+    // remove any class we may have added before
+    $(this).removeClass("past present future");
+    // color block based on past, present, future class
+    if (pastHour < currentHour) {
+      $(this).addClass("past");
+    } else if (pastHour > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+}
+
+function handleSave(event) {
+  // get parent id
+  var hourId = $(this).parent().attr("id");
+  // save localstorage in textarea
+  localStorage.setItem(moment().format("DDDYYYY") + hourId, $("#" + hourId + " textarea").val());
+}
